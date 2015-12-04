af97877b5db672465ba9a49d2e7ea506db197110
Jose Vicente Fernandez Ferrer <roninmcr@gmail.com>
2015-12-04 18:01:54 -0430

    Solve the second part of the day 04 challenge

--- day04/main.py
+++ day04/main.py
@@ -8,9 +8,11 @@ import hashlib
 import itertools
 
 
-def find_minimum_number_for_md5(input_string):
+def find_minimum_number_for_md5(input_string, number_of_zeroes):
+    result_string = "0" * number_of_zeroes
     for i in itertools.count():
         test_string = "".join([input_string, str(i)])
-        if "00000" == hashlib.md5(test_string).hexdigest()[:5]:
+        if (result_string ==
+                hashlib.md5(test_string).hexdigest()[:number_of_zeroes]):
             break
     return i
--- day04/tests.py
+++ day04/tests.py
@@ -10,13 +10,14 @@ from day04.main import find_minimum_number_for_md5
 class Test(unittest.TestCase):
 
     def test_avent_coins_default(self):
-        self.assertEqual(609043, find_minimum_number_for_md5("abcdef"))
+        self.assertEqual(609043, find_minimum_number_for_md5("abcdef", 5))
 
     def test_avent_coins_final(self):
-        self.assertEqual(1048970, find_minimum_number_for_md5("pqrstuv"))
+        self.assertEqual(1048970, find_minimum_number_for_md5("pqrstuv", 5))
 
     def test_avent_coins_answer(self):
-        self.assertEqual(254575, find_minimum_number_for_md5("bgvyzdsv"))
+        self.assertEqual(254575, find_minimum_number_for_md5("bgvyzdsv", 5))
+        self.assertEqual(1038736, find_minimum_number_for_md5("bgvyzdsv", 6))
 
 if __name__ == "__main__":
     unittest.main()
